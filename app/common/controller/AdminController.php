91eac363718e245d7741963c2b944ec7837d68a1
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2025-01-15 09:28:15 +0800

    refactor(view): remove redundant template path generation
    - Remove unnecessary code that generates a template path based on the request pathinfo
    - Simplify the render function by removing the conditional block that sets the template path

--- app/common/controller/AdminController.php
+++ app/common/controller/AdminController.php
@@ -133,9 +133,6 @@ class AdminController extends BaseController
     {
         if ($layout) View::instance()->engine()->layout('/layout/default');
         View::assign($vars);
-        if (empty($template)) {
-            $template = strtolower(str_replace('.', '/', $this->request->pathinfo()));
-        }
         return View::fetch($template);
     }
 
