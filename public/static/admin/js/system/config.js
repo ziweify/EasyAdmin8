4bbe28762641da71c99a7ec17147f2cbb9790082
wolfcode <37436228+wolf-leo@users.noreply.github.com>
2025-03-17 18:02:06 +0800

    refactor(system): upgrade tabs functionality and improve error handling
    - Replace element.on with tabs.on for better tab management Add success and error handling for form submission
    - Update HTML structure to use layui-tabs for improved UI
    - Remove unnecessary Vue import

--- public/static/admin/js/system/config.js
+++ public/static/admin/js/system/config.js
@@ -1,15 +1,15 @@
-define(["jquery", "easy-admin", "vue"], function ($, ea, Vue) {
+define(["jquery", "easy-admin"], function ($, ea) {
 
     var form = layui.form;
 
     return {
         index: function () {
             var _group = 'site'
-            var element = layui.element;
-            element.on('tab(docDemoTabBrief)', function (data) {
+            let tabs = layui.tabs
+            var TABS_ID = 'docDemoTabBrief';
+            tabs.on(`afterChange(${TABS_ID})`, function (data) {
                 _group = $(this).data('group')
-            });
-
+            })
             let _upload_type = upload_type || 'local'
             $('.upload_type').addClass('layui-hide')
             $('.' + _upload_type).removeClass('layui-hide')
@@ -20,12 +20,15 @@ define(["jquery", "easy-admin", "vue"], function ($, ea, Vue) {
                 $('.' + _upload_type).removeClass('layui-hide')
             });
 
-
             form.on("submit", function (data) {
                 data.field['group'] = _group
             });
 
-            ea.listen();
+            ea.listen('', function (res) {
+                ea.msg.success(res.msg);
+            }, function (err) {
+                ea.msg.error(err.msg);
+            });
         }
     };
 });
